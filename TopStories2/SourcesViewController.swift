a5168860a6417a1a245d78de10c48a68498e79cd
Mosahaq <Mosahaq@gmail.com>
2020-01-29 10:45:23 -0600

    Set up API protocols

--- TopStories2/SourcesViewController.swift
+++ TopStories2/SourcesViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class SourcesViewController: UITableViewController {
+class SourcesViewController: UITableViewController
+{
 
-    override func viewDidLoad() {
+     var sources = [[String: String]]()
+     let apiKey = "5d892509a49046a087917c466fa80d09"
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "News Sources"
+        let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
     }
-
 
 }
 
